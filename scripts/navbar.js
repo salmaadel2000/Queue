46ee88bd90673e18602d86287c26e15e09b35996
Salma Adel <Salma.Adel@alqemam.com>
2024-08-25 16:41:37 +0200

    salma-push-nav-height

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,11 +1,14 @@
 function toggleScroll() {
     const body = document.body;
+    const containerNavMain = document.querySelector('.container-nav-main');
     const navbarToggler = document.querySelector('.navbar-toggler');
     const isExpanded = navbarToggler.getAttribute('aria-expanded') === 'true';
-
-    if (isExpanded) {
+    const maxWidth = window.matchMedia("(max-width: 990px)");
+    if (isExpanded && maxWidth.matches) {
+        containerNavMain.style.height = '100vh';
         body.style.overflow = 'hidden';
     } else {
-        body.style.overflow = '';
+        containerNavMain.style.height = ''; 
+        body.style.overflow = ''; 
     }
 }
